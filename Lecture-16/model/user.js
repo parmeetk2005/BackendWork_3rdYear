ce33529e7b652d0bfafcfd558bc8e21c6c7f647a
parmeetk2005 <parmeetk2005@gmail.com>
2025-08-12 14:12:58 +0530

    Lecture-16 assignment

--- Lecture-16/model/user.js
+++ Lecture-16/model/user.js
@@ -1,1 +1,19 @@
 // email, username, password
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const BlogPost = new Schema({
+//   title: String,
+//   body: String,
+//   date: Date
+// });
+
+
+// module.exports = mongoose.model('Blog', BlogPost);
+
+//creating a usee schema
+const userSchema=new Schema({
+  email:String,
+  username:String,
+  password: String
+})
+module.exports=mongoose.model('User',userSchema);
