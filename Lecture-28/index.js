256a547a6e47e119bee3f4278217f15c02acafd6
parmeetk2005 <parmeetk2005@gmail.com>
2025-09-15 15:50:41 +0530

    Lecture-28 notes

---
+++ Lecture-28/index.js
@@ -0,0 +1,22 @@
+const { PrismaClient } = require('./generated/prisma');
+const prisma = new PrismaClient()
+async function addUser(email, name, password) {
+    await prisma.user.create({
+        data:{
+            email: email,
+            name: name,
+            password: password
+        }
+    })
+}
+// addUser("kaurparmeet@gmail.com", "Parmeet Kaur", "1234").then(()=>{
+//     console.log("User added")
+// })
+
+async function getAllUser(){
+    let allUser = await prisma.user.findMany()
+    return allUser
+}
+getAllUser().then((data)=>{
+    console.log(data)
+})
